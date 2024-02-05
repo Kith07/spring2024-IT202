ff8f9e71b887b246f6e438fd21984296daf4a035
Likhith <lm457@njit.edu>
2024-02-05 14:45:50 -0500

    finished problem 1

--- public_html/problem2.php
+++ public_html/problem2.php
@@ -9,8 +9,12 @@ function getTotal($arr) {
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO do adding here
     //TODO do rounding stuff here (round to two decimals i.e., 0.10, 0.01, 0.00)
+    for ($x = 0; $x < count($arr); $x++) {
+        $total += $arr[$x];
+        round($total, 3);
+    }
     echo "The total is " . var_export($total, true);
-}
+    }
 echo "Problem 2: Adding Floats<br>";
 ?>
 <table>
--- public_html/problem3.php
+++ public_html/problem3.php
@@ -10,6 +10,10 @@ function bePositive($arr) {
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
     //hint: may want to use var_dump() or similar to show final data types
+    for ($x = 0; $x < count($arr); $x++) {
+        abs($arr[$x]);
+    }
+    var_dump($arr);
 }
 echo "Problem 3: Be Positive<br>";
 ?>
