cc7b41d43e8a213c69f16aa58b79ac8962b4a11a
Likhith <lm457@njit.edu>
2024-02-05 14:59:13 -0500

    added ucid to problem 1

--- public_html/problem1.php
+++ public_html/problem1.php
@@ -8,11 +8,14 @@ function processArray($arr) {
     echo "<br>Odds output:<br>";
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+
     for ($x = 0; $x < count($arr); $x++) {
         if($arr[$x] % 2 != 0){
             echo $arr[$x] . "<br>";
         }
     }
+
+    echo "LM457";
 
 }
 echo "Problem 1: Odd Output<br>";
