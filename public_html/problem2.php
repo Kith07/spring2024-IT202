0a87a5b77eb857b4d9a13c97c27a33a3493c9d7a
Likhith <lm457@njit.edu>
2024-02-05 19:29:33 -0500

    completed problem 2 i think

--- public_html/problem2.php
+++ public_html/problem2.php
@@ -9,11 +9,13 @@ function getTotal($arr) {
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO do adding here
     //TODO do rounding stuff here (round to two decimals i.e., 0.10, 0.01, 0.00)
-    for ($x = 0; $x < count($arr); $x++) {
-        $total += $arr[$x];
-        round($total, 3);
-    }
+
+    //UCID: LM457 DATE: 2/5/2024
+    $total = array_sum($arr);
+    $toral = round($total, 2);
+
     echo "The total is " . var_export($total, true);
+    echo "<br>UCID: LM457";
     }
 echo "Problem 2: Adding Floats<br>";
 ?>
